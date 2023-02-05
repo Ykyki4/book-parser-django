dd692803175478f53c55e177fbc3417d303031ed
Ykyki4 <jimmypantalones@gmail.com>
2023-02-05 22:58:18 +0300

    move djagno admin static to frontend static

--- tululu/settings.py
+++ tululu/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
